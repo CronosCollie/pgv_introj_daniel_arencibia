4c299ca858cc589fff5af16274ba4dc41e92a365
YerayRomano <benfiquista2904@gmail.com>
2022-09-14 13:42:22 +0100

    propiedad de cantidad mínima en la cafetera

--- Clases_UT03/src/clases_ut03/Cafetera.java
+++ Clases_UT03/src/clases_ut03/Cafetera.java
@@ -11,6 +11,7 @@ package clases_ut03;
 public class Cafetera {
     private int capacidadMaxima;
     private int cantidadActual;
+    private int cantidadMinima;
     
     public Cafetera(){
         capacidadMaxima = 1000;
